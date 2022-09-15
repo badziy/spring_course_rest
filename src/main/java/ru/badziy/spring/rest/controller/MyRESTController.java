5488098632fd6bccd496691f6207c0e46a1e1da5
Badziev Alan <badjity@yandex.ru>
2022-09-15 18:17:06 +0300

    Add UDATE and DELETE methods

--- src/main/java/ru/badziy/spring/rest/controller/MyRESTController.java
+++ src/main/java/ru/badziy/spring/rest/controller/MyRESTController.java
@@ -42,4 +42,22 @@ public class MyRESTController {
         return employee;
     }
 
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+        employeeService.saveEmployee(employee);
+        return employee;
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public String deleteEmployee(@PathVariable int id) {
+        Employee employee = employeeService.getEmployee(id);
+
+        if (employee == null) {
+            throw new NoSuchEmployeeException("There is no Employee with ID = " + id + " in Database");
+        }
+
+        employeeService.deleteEmployee(id);
+        return "Employee with ID = " + id + " was deleted";
+    }
+
 }
